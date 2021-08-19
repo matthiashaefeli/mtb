37e9304d8f49b5d854ebbfee1945b93720ecd63f
matthiashaefeli <haefeli.matthias2gmail.com>
2021-08-19 18:57:46 -0500

    change name to journey

--- app/javascript/packs/travel.js
+++ app/javascript/packs/travel.js
@@ -1,11 +1,1 @@
-$(document).ready(function() {
-  $('.location_form, .comment_form').hide();
 
-  $('#add_new_location').click(function() {
-    $('.location_form').toggle();
-  })
-
-  $('#add_new_comment').click(function() {
-    $('.comment_form').toggle();
-  })
-});
